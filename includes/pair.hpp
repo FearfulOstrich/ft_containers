7bd74fd4a639c99a6bfa7e37c8592f4d839d3f7d
aalleon <antoine.alleon@gmail.com>
2022-10-13 16:35:56 +0200

    typedefs.

--- includes/pair.hpp
+++ includes/pair.hpp
@@ -6,7 +6,7 @@
 /*   By: aalleon <aalleon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 16:13:16 by aalleon           #+#    #+#             */
-/*   Updated: 2022/10/13 11:29:31 by aalleon          ###   ########.fr       */
+/*   Updated: 2022/10/13 15:19:25 by aalleon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ namespace ft
 	template< typename T1, typename T2 >
 	struct pair
 	{
-		// Attributes
-		T1	first;
-		T2	second;
-
 		// Typedefs
 		typedef T1	first_type;
 		typedef T2	second_type;
 
+		// Attributes
+		first_type	first;
+		second_type	second;
+
 		// Constructors
 		pair( void );
-		pair( const T1& x, const T2& y );
+		pair( const first_type& x, const second_type& y );
 		template< typename U1, typename U2 >
 		pair( const pair< U1, U2 >& other );
 		
@@ -40,6 +40,7 @@ namespace ft
 	pair< T1, T2 > make_pair( T1 t, T2 u );
 
 	# include "pair.tpp"
+
 }
 
 #endif
